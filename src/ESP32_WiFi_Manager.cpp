345dce8df3c1b6b989712782e651c25b450a422d
Brian Willy <bwilly@prescribewellness.com>
2024-09-24 22:08:22 -0400

    properly upgraded ElegantOTA

--- src/ESP32_WiFi_Manager.cpp
+++ src/ESP32_WiFi_Manager.cpp
@@ -74,7 +74,7 @@ With ability to map DSB ID to a name, such as raw water in, post air cooler, pos
 #define AP_REBOOT_TIMEOUT 600000 // 10 minutes in milliseconds
 unsigned long apStartTime = 0;   // Variable to track the start time in AP mode
 
-#define version "esp32:Sept-2024:full-clean"
+#define version "esp32:Sept-2024:single-core" // trying to identify cause of unreliable dht22 readings
 
 // MQTT Server details
 // const char *mqtt_server = "192.168.68.120"; // todo: change to config param
@@ -650,6 +650,20 @@ void setup()
       request->send(200, "text/plain", "Done. ESP will restart, connect to your AP");
       delay(mainDelay.toInt()); // delay(3000);
       ESP.restart(); });
+
+    // Correct the onEnd function signature
+    ElegantOTA.onEnd([](bool success)
+                     {
+    if (success) {
+        Serial.println("OTA End - restarting device.");
+        ESP.restart();  // Restart the ESP32 after OTA is completed.
+    } else {
+        Serial.println("OTA failed.");
+    } });
+
+    // Correct the onProgress function signature (optional, depending on version)
+    ElegantOTA.onProgress([](unsigned int progress, unsigned int total)
+                          { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
 
     // uses path like server.on("/update")
     ElegantOTA.begin(&server);
